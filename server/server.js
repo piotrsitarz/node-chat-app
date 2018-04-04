03d08b212926a88a22ea1126e98947925b9a9aea
Piotr Sitarz <piotrsitarz@onet.eu>
2018-04-04 22:20:09 +0200

    Sens messages to only people in same room.

--- server/server.js
+++ server/server.js
@@ -79,13 +79,17 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message, callback) =>{
         console.log('Create message:', message);
+        let user = users.getUser(socket.id);
+        if (user && isRealString(message.text)) {
+          io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
         // io.emit emits event to a every single connection
         // io.emit('newMessage', {
         //   from: message.from,
         //   text: message.text,
         //   createdAt: new Date().getTime()
         // })
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        // io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
         // socket.broadcast.emit('newMessage', {
         //   from: message.from,
@@ -99,7 +103,10 @@ io.on('connection', (socket) => {
     // });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        let user = users.getUser(socket.id);
+        if (user) {
+          io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
   socket.on('disconnect', () =>{
